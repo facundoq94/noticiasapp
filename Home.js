3322c997c159552bc12f4bdc2e6b3853f6adc254
facundoq94 <facunquintana@gmail.com>
2019-12-17 10:48:10 -0300

    probando vscode

--- Home.js
+++ Home.js
@@ -12,7 +12,7 @@
      title='GORILLAZ EN CORRIENTES'
      image={{uri: "https://ep01.epimg.net/elpais/imagenes/2017/03/27/tentaciones/1490603139_309528_1490603385_noticia_normal.jpg"}}>
      <Text style={{marginBottom: 10}}>
-       The idea with React Native Elements is more about component structure than actual design.
+       The idea with React Native Elements is more about component structure than actual design. jaja
      </Text>
    </Card>
     )
